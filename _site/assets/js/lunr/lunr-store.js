f2b3d67c4cc42446420d93408d54e4d8c576f284
Adam Dubey <adubey0311@gmail.com>
2020-03-16 16:44:56 -0500

    [AD] Blanket refactoring to address remote theme issue

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -7,5 +7,5 @@ var store = [{
         "title": "Deploying a React.js app with GitHub Pages",
         "excerpt":"A journey of building and deploying a React.js application with GitHub Pages. With a few hours to kill on a lazy Sunday afternoon… GitHub Pages makes it very easy to quickly deploy a web application that only needs a front-end, which is a great medium to use if you for...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/projects/2019-09-15-deploying-a-react-app-to-github-pages/",
-        "teaser":null}]
+        "url": "http://localhost:4000/deploying-a-react-app-to-github-pages/",
+        "teaser":null},]
